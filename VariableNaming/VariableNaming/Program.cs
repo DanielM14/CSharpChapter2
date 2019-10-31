b3f159f73ebb03aaa85fee3f6bd24afcd794767a
DanielM14 <37811239+DanielM14@users.noreply.github.com>
2019-10-31 15:21:43 -0500

    update

---
+++ VariableNaming/VariableNaming/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableNaming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 15;
+            int resultAdd;
+            int resultMinus;
+            int resultDivide;
+            int resultMultiply;
+            int resultModulus;
+
+            resultAdd = num1 + num2;
+            resultMinus = num1 - num2;
+            resultDivide = num1 / num2;
+            resultMultiply = num1 * num2;
+            resultModulus = num1 % num2;
+
+            Console.WriteLine("The result of ${0} + ${1} = ${2} ", num1, num2, resultAdd);
+            Console.WriteLine("The result of ${0} - ${1} = ${2} ", num1, num2, resultMinus);
+            Console.WriteLine("The result of ${0} / ${1} = ${2} ", num1, num2, resultDivide);
+            Console.WriteLine("The result of ${0} * ${1} = ${2} ", num1, num2, resultMultiply);
+            Console.WriteLine("The result of ${0} % ${1} = ${2} ", num1, num2, resultModulus);
+
+        }
+    }
+}
