b77fe01ac772b49f7542c84615dbd69a9d02ef2b
DanielM14 <37811239+DanielM14@users.noreply.github.com>
2019-11-01 15:23:25 -0500

    update debugging

---
+++ DebugTwo2/DebugTwo2/Program.cs
@@ -0,0 +1,22 @@
+﻿// This program greets the user
+// and multiplies two entered values
+using System;
+using static System.Console;
+class DebugTwo2
+{
+    static void Main()
+    {
+        string name;
+        string firstString, secondString;
+        int first, second, product;
+        WriteLine("Enter your name");
+        name = ReadLine();
+        WriteLine("Hello {0}! Enter an integer", name);
+        firstString = ReadLine();
+        WriteLine("Enter another integer");
+        secondString = ReadLine();
+        product = first * second;
+        WriteLine("Thank you {1}. The product of {2} and {3} is {4}",
+       name, firstString, secondString, product);
+    }
+}
